afe7f279e564c83d8cb40ec1fe1c066546a3adf7
= <=>
2018-01-21 16:48:55 -0600

    fix something

--- js/common/Lights.js
+++
@@ -1,7 +0,0 @@
-var DirectionalLight = function(dir, color)
-{
-    return {
-        direction: dir,
-        color: color,
-    }
-}
--- js/common/Material.js
+++
@@ -1,9 +0,0 @@
-var Material = function(diffuse, specular, specExponent, hasNormalMap)
-{
-    return {
-        diffuse: diffuse,
-        specular: specular,    
-        specExponent : specExponent,
-        hasNormalMap : hasNormalMap,
-    }
-}
